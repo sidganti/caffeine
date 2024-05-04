c6a4704772553cc7469925f318120be766d60ec1
Siddharth Ganti <gantisid@gmail.com>
2024-05-04 10:30:08 -0400

    Improved thread collision and runtime accuracy

--- caffeine.py
+++ caffeine.py
@@ -42,17 +42,18 @@ def caffeine(runtime: int = 0) -> None:
         """
         Moves mouse to random position on screen
         """
-        print('thread registered')
+        print('thread registered', time.monotonic())
         pos_x = random.randint(0, SCREEN_WIDTH)
         pos_y = random.randint(0, SCREEN_HEIGHT)
         duration = random.randint(1, 5)
         tween = random.choice(TWEENING_FUNCTIONS)
+        print('duration', duration)
 
         try:
             pyautogui.moveTo(pos_y, pos_x, duration, tween)
         except pyautogui.FailSafeException:
             print('killed by mouse')
-        print('thread killed')
+        print('thread killed', time.monotonic())
         quit()
 
     def key_pressed(key) -> None:
@@ -67,7 +68,8 @@ def caffeine(runtime: int = 0) -> None:
 
     end_time = time.monotonic() + runtime
     while under_runtime():
-        interval = random.randint(1, 5)
+        interval = random.randint(5, 10)
+        print('interval', interval)
         interval_time = time.monotonic() + interval
 
         # move_mouse()
@@ -75,7 +77,7 @@ def caffeine(runtime: int = 0) -> None:
         mouse_thread.daemon = True
         mouse_thread.start()
 
-        while time.monotonic() < interval_time:
+        while time.monotonic() < interval_time and under_runtime():
             if not listener.is_alive():
                 print('killed by key')
                 quit()
@@ -83,6 +85,8 @@ def caffeine(runtime: int = 0) -> None:
             #     print('killed by mouse')
             #     quit()
             time.sleep(0.1)
+        else:
+            mouse_thread.join()
     else:
         print('killed by time')
         quit()
