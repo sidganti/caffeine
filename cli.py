e397a9a3e93a01e93fbe74f3ecc78f80727a63a1
Siddharth Ganti <gantisid@gmail.com>
2024-05-04 22:56:03 -0400

    Expand existing command line arguments

--- cli.py
+++ cli.py
@@ -9,12 +9,23 @@ from caffeine import caffeine
 def main():
     parser = argparse.ArgumentParser(
         prog='Caffeine',
+        conflict_handler='resolve',
         description='Command-line tool for keeping your computer awake',
         epilog="""
             Terminate at any time by pressing any key
         """
     )
-    parser.add_argument(
+    runtime_group = parser.add_mutually_exclusive_group()
+    runtime_group.add_argument(
+        'runtime',
+        nargs='?',
+        default=0,
+        type=int,
+        choices=range(0, 86400),
+        help='duration you want the program to run (0 runs indefinitely) [default=0]',
+        metavar='[0-86400]'
+    )
+    runtime_group.add_argument(
         '-t', '--time',
         default=0,
         type=int,
@@ -26,11 +37,13 @@ def main():
         '-c', '--hotcorners',
         default=False,
         action='store_true',
-        help='duration you want the program to run (0 runs indefinitely) [default=0]',
+        help='Prevents mouse from reaching corners of display',
     )
     args = parser.parse_args()
 
-    caffeine(args.time, args.hotcorners)
+    runtime = args.time if args.runtime == 0 and args.time != 0 else args.runtime
+
+    caffeine(runtime, args.hotcorners)
 
 
 if __name__ == '__main__':
